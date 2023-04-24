15b92abe9bb72dde60f4cdb5b02cd558b91911a8
Jelle Kootwijk <kootwijkjelle@gmail.com>
2023-04-24 19:02:45 +0200

    Add sidebar
    Add sidebar

--- js/concept/track.js
+++ js/concept/track.js
@@ -31,12 +31,14 @@ function showDisplayButtons(displayNumber, arrPos)
 function showDisplayAllButtons()
 {
 	$('#all-displays').append(
-		`<h4>All panels</h4>
-		<div class="buttons column">
-			<button class="btn btn-grey" onclick="DisplayManager.triggerDisplay('blank', ${arrPos})">OFF</button>
-			<button class="btn btn-green" onclick="DisplayManager.triggerDisplay('green', ${arrPos})">Green</button>
-			<button class="btn btn-yellow" onclick="DisplayManager.triggerDisplay('yellow', ${arrPos})">Yellow</button>
-			<button class="btn btn-red" onclick="DisplayManager.triggerDisplay('red', ${arrPos})">Red</button>
+		`<div class="info ml-5 mt-8">
+			<h4>All panels</h4>
+			<div class="buttons column">
+				<button class="btn btn-grey" onclick="DisplayManager.triggerDisplay('blank', 'all')">OFF</button>
+				<button class="btn btn-green" onclick="DisplayManager.triggerDisplay('green', 'all')">Green</button>
+				<button class="btn btn-yellow" onclick="DisplayManager.triggerDisplay('yellow', 'all')">Yellow</button>
+				<button class="btn btn-red" onclick="DisplayManager.triggerDisplay('red', 'all')">Red</button>
+			<div>
 		<div>`
 	);
 }
