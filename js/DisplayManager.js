ed88ccacbc552b96be170555a1aec59591901268
Jelle Kootwijk <47365481+SpoOkyJelle@users.noreply.github.com>
2023-05-11 22:41:01 +0200

    Added border color change when pressed
    Added API test methods

--- js/DisplayManager.js
+++ js/DisplayManager.js
@@ -35,6 +35,37 @@ class DisplayManager {
     }
 
     static #sendRequest(display, ipPos) {
+        $.ajax({
+            // Url for testing purposes.
+            // url: 'http://localhost/api-test/' + display + '.php',
+            url: this.#ips[ipPos] + "/" + display,
+            type: "application/json",
+            method: 'get',
+            success: function(result){
+                let buttonColor;
+
+                switch (result[0].color) {
+                    case 'green':
+                        buttonColor = '#1ca75d';
+                        break;
+                    case 'blue':
+                        buttonColor = '#3d7bf9';
+                        break;
+                    case 'yellow':
+                        buttonColor = '#FFE553';
+                        break;
+                    case 'red':
+                        buttonColor = '#ED4646';
+                        break;
+                    case 'blank':
+                        buttonColor = '#707070';
+                        break;
+                }
+
+                $('#display' + ipPos).css('border', '5px solid ' + buttonColor)
+            }
+        })
+
     	console.log(display, this.#ips[ipPos]);
     //    var xhr = new XMLHttpRequest();
     //    xhr.open("GET", this.#ips[ipPos] + "/" + display, true);
