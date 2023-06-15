b99ddf376c62868e1397a4493522c30ce91dd173
Jelle Kootwijk <47365481+SpoOkyJelle@users.noreply.github.com>
2023-06-15 22:12:00 +0200

    Init background

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/templates/track.html');
+});
+
+app.listen(8081, function() {
+    console.log('TrackAlert online');
+});
