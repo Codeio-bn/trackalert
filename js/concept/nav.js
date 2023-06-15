75be795b1a915c661ebc78a9e85afa297c216bc4
Jelle Kootwijk <47365481+SpoOkyJelle@users.noreply.github.com>
2023-06-15 21:34:09 +0200

    Start fresh

--- js/concept/nav.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-	document.getElementById("connections-sidenav").style.width = "550px";
-	$('#overlay').css('display', 'block');
-}
-
-function closeNav() {
- 	document.getElementById("connections-sidenav").style.width = "0";
-	$('#overlay').css('display', 'none');
-}
